65d98859f17c675ef1dd21c8814fb701d1e19cf1
Rick Anderson <riande@microsoft.com>
2019-01-21 14:05:28 -1000

    next version

--- TireHtP/TireHtP/Methods.cs
+++ TireHtP/TireHtP/Methods.cs
@@ -6,25 +6,36 @@ namespace TireHtP
     {
         public static double Lift(double Height, double Weight, double PSI, double Width)
         {
+            // b is tire patch length/2. See tire image a bottom of file.
             var b = B(Weight, PSI, Width);
             return Math.Round(Math.Sqrt(Height * Height / 4.0 - b*b ), 2);
         }
 
+        public static double PatchLength(double Weight, double PSI, double Width)
+        {
+            var pl = Weight / PSI / Width;
+            var diff = pl - 25.287356321839081;
+            return pl;
+            // return Weight / PSI / Width;
+        }
         public static double B(double Weight, double PSI, double Width)
         {
-            return Weight / PSI / Width / 2.0;
+            // Divide by 2 to get the right triangle B of the tire patch lenghth.
+            return PatchLength(Weight, PSI, Width) / 2;
         }
 
-        public static (string lift, string strDiff, double diff) LiftAndDiff(double Height, double Weight, double PSI, double Width, 
-            double WheelDiameter)
+        public static (string lift,  string strDiff, double diff, string patchLen) 
+            LiftAndDiff(double Height, double Weight, double PSI, double Width,  double WheelDiameter)
         {
-            var lift = Methods.Lift(Height, Weight, PSI, Width);
+            var lift = Lift(Height, Weight, PSI, Width);
             var diff = Math.Round(Height / 2.0 - lift, 2);
-
-            return StrLiftAndDiff(diff, lift, WheelDiameter);
+            //string patchLen = (2 * B(Weight, PSI, Weight)).ToString();
+            string patchLen = string.Format("{0:0.00}", PatchLength(Weight, PSI, Width));
+            return StrLiftAndDiff(diff, lift, WheelDiameter, patchLen);
         }
 
-        public static (string strLift, string strDiff, double diff) StrLiftAndDiff(double Diff, double Lift, double WheelDiameter)
+        public static (string strLift, string strDiff, double diff, string patchLen) 
+            StrLiftAndDiff(double Diff, double Lift, double WheelDiameter, string PatchLen)
         {
             string str_Diff = Diff.ToString();
             string str_Lift = (Lift > (WheelDiameter / 2.0 - .5) || Lift.ToString().Equals("NaN") != true)
@@ -33,8 +44,24 @@ namespace TireHtP
             if (str_Lift.Equals("Rim"))
             {
                 str_Diff = "X";
+                PatchLen = "X";
             }
-            return (str_Lift, str_Diff, Diff);
+            return (str_Lift, str_Diff, Diff, PatchLen);
         }
     }
 }
+
+/*
+
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,            /|             ,
+ ,  Height /   |            ,
+  ,      /     |Lift        ,
+   ,   /       |           ,
+     ,------B------------ '
+       ' - , _ _ _ ,  '
+*/
